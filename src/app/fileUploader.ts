b279e913893bac6ba68926756cb35917c1c6e1e3
Chris Richards <chris@myrelaytech.com>
2024-09-19 13:15:58 -0500

    fix(layout): env

--- src/app/fileUploader.ts
+++ src/app/fileUploader.ts
@@ -5,8 +5,8 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 const s3Config = {
     bucketName: process.env.AWS_BUCKET_NAME as string,
     region: process.env.AWS_REGION as string,
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID_MAIN as string,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_MAIN as string
 }
 
 export const UploadDoc = async (buffer: Buffer, key: string) => {
