9e9ad748019d9948da286ecb7fa4fca541f32068
Chris Richards <chris@myrelaytech.com>
2024-09-19 12:48:18 -0500

    fix(layout): build fix

--- src/types/next.ts
+++ src/types/next.ts
@@ -1,16 +1,1 @@
 import { NextApiResponse } from "next";
-import type { Server as HTTPServer } from 'http'
-import type { Socket as NetSocket } from 'net'
-import type { Server as IOServer } from 'socket.io'
-
-export interface NextApiResponseWithSocket extends NextApiResponse {
-  socket: SocketWithIO
-}
-
-export interface SocketServer extends HTTPServer {
-  io?: IOServer | undefined
-}
-
-export interface SocketWithIO extends NetSocket {
-  server: SocketServer
-}
