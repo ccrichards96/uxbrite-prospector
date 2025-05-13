4d75d6f5181a06165e4dd36bfa85692cb1da69ae
Chris Richards <chris@myrelaytech.com>
2025-05-13 15:09:18 -0500

    build

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,4 @@
 import { defineBackend } from "@aws-amplify/backend";
-import { screenshot } from './screenshot/resource';
-
 // defineBackend({
 //   screenshot,
 // });
--- amplify/screenshot/resource.ts
+++
@@ -1,6 +0,0 @@
-import { defineFunction } from "@aws-amplify/backend";
-    
-export const screenshot = defineFunction({
-  name: "screenshot",
-  entry: "./handler.ts"
-});
